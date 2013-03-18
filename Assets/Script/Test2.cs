21ef90e4607a49bcd73c3af430b7a461cc24c5cd
Vlad Mironov <vladd3d@gmail.com>
2013-03-18 14:57:08 +0400

    123

---
+++ Assets/Script/Test2.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class Test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void NewMethod()
+    {
+        Object oo = new Object();
+    }
+
+    void NewMethod123()
+    {
+        Object oo = new Object();
+    }
+	void NewMethod321()
+    {
+        Object oo = new Object();
+    }
+}
